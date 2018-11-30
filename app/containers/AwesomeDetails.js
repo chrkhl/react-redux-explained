a4f14fc36b46c611ba8f003337fcc6de16d2d101
chrkhl <kuehlchristian@icloud.com>
2018-11-30 19:11:35 +0100

    routing: add second container component -> AwesomeDetails

---
+++ app/containers/AwesomeDetails.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AwesomeDetails = props => (
+  <>
+    <h1>Awesome Details</h1>
+    props: { JSON.stringify(props) }
+  </>
+);
+
+export default AwesomeDetails;
