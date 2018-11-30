7bdd99c42154e074d1f0c470f8106dc64d04d97a
chrkhl <kuehlchristian@icloud.com>
2018-11-30 14:29:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div className='content'>
+    <div className='container'>
+      <h1>Awesome List</h1>
+    </div>
+  </div>,
+  document.getElementById('app')
+);
