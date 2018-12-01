ebfe5c153a3206a929e26e1ebea146c738befedf
chrkhl <kuehlchristian@icloud.com>
2018-12-01 14:06:40 +0100

    show real data in AwesomeDetails

--- app/containers/AwesomeDetails.js
+++ app/containers/AwesomeDetails.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import { navigateToRoute } from '../AwesomeRouter';
 
-const staticDetails = {
-  "id": "4711",
-  "name": "Max Mustermann",
-  "details": {
-    "avatar": "https://pickaface.net/gallery/avatar/unr_funny_170108_2338_7hs7qcl1.png",
-    "age": 32,
-    "interests": [
-      "Design", "Kunst", "Reisen"
-    ]
-  }
+const AwesomeDetails = props => {
+  const personData = props.data.find(person => person.id === props.personId);
+  
+  return (
+    <div className='details-view'>
+      <h1>Awesome Details</h1>
+      
+      { !personData &&
+        <p>Keine Informationen zu der Person mit ID { props.personId } gefunden!</p>
+      }
+      
+      { personData &&
+        <div className='person-profile'>      
+          <img className='avatar' src={ personData.details.avatar } alt='avatar' />
+          <dl>
+            <dt>Name</dt>
+            <dd>{ personData.name }</dd>
+            <dt>Nummer</dt>
+            <dd>{ props.personId }</dd>
+            <dt>Alter</dt>
+            <dd>{ personData.details.age }</dd>
+            <dt>Interessen</dt>
+            <dd>
+              <ul>
+                { personData.details.interests.map((interest, index) => <li key={index}>{interest}</li>)}
+              </ul>
+            </dd>
+          </dl>
+        </div>
+      }
+      
+      <p>
+        <button onClick={() => navigateToRoute('list')}>
+          Zurück zur Liste
+        </button>
+      </p>
+    </div>
+  );
 };
 
-const AwesomeDetails = props => (
-  <div className='details-view'>
-    <h1>Awesome Details</h1>
-    
-    <div className='person-profile'>      
-      <img className='avatar' src={ staticDetails.details.avatar } alt='avatar' />
-      <dl>
-        <dt>Name</dt>
-        <dd>{ staticDetails.name }</dd>
-        <dt>Nummer</dt>
-        <dd>{ props.personId }</dd>
-        <dt>Alter</dt>
-        <dd>{ staticDetails.details.age }</dd>
-        <dt>Interessen</dt>
-        <dd>
-          <ul>
-            { staticDetails.details.interests.map((interest, index) => <li key={index}>{interest}</li>)}
-          </ul>
-        </dd>
-      </dl>
-    </div>
-    
-    <p>
-      <button onClick={() => navigateToRoute('list')}>
-        Zurück zur Liste
-      </button>
-    </p>
-  </div>
-);
+
 
 export default AwesomeDetails;
