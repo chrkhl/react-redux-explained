ffb9d3d074e7c99a72bf0c4e125dcfcb0421534d
chrkhl <kuehlchristian@icloud.com>
2018-12-01 14:03:46 +0100

    pimp our data structure

--- index.js
+++ index.js
@@ -6,16 +6,44 @@ import AwesomeRouter from './app/AwesomeRouter';
 
 const data = [{
     "id": "4711",
-    "name": "Max Mustermann"
+    "name": "Max Mustermann",
+    "details": {
+      "avatar": "https://pickaface.net/gallery/avatar/unr_funny_170108_2338_7hs7qcl1.png",
+      "age": 32,
+      "interests": [
+        "Design", "Kunst", "Reisen"
+      ]
+    }
   },{
     "id": "4712",
-    "name": "Sabine Mustermann"
+    "name": "Sabine Mustermann",
+    "details": {
+      "avatar": "https://pickaface.net/gallery/avatar/unr_qwerty_161123_0418_9ndl6z.png",
+      "age": 28,
+      "interests": [
+        "Bücher", "Reiten", "Kochen"
+      ]
+    }
   },{
     "id": "4713",
-    "name": "John Doe"
+    "name": "John Doe",
+    "details": {
+      "avatar": "https://pickaface.net/gallery/avatar/unr_mrz34_171220_0103_2h8btat.png",
+      "age": 36,
+      "interests": [
+        "Sport", "Zauberei", "Lesen"
+      ]
+    }
   },{
     "id": "4714",
-    "name": "Jane Doe"
+    "name": "Jane Doe",
+    "details": {
+      "avatar": "https://pickaface.net/gallery/avatar/20121027_064623_1221_Maxanakz1.png",
+      "age": 29,
+      "interests": [
+        "Yoga", "Handarbeit", "Martial Arts"
+      ]
+    }
   }];
 
 ReactDOM.render(
