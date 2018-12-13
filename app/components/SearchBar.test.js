4e76fa0ee030df86375aee72a661fd490d35f620
chrkhl <kuehlchristian@icloud.com>
2018-12-13 11:42:30 +0100

    testing: find component child and access attributes/props

--- app/components/SearchBar.test.js
+++ app/components/SearchBar.test.js
@@ -9,4 +9,16 @@ describe('SearchBar', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<SearchBar />);
   });
+  
+  it('renders search term passed via props', () => {
+    const searchTerm = 'wisdom';
+    const wrapper = shallow(<SearchBar searchTerm={ searchTerm } />);
+    
+    const input = wrapper.find('input[type="text"]');
+    
+    // access attribute via props().myProp
+    expect(input.props().value).toBe(searchTerm);
+    // or .props('myProp')
+    expect(input.prop('value')).toBe(searchTerm);
+  })
 });
